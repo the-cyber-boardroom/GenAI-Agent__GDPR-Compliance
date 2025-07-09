5cdb7aaf1a528fed5c001090a81b267f684df31b
Dinis Cruz <dinis.cruz@owasp.org>
2025-07-09 18:03:33 +0100

    added first methods and tests to AWS__Setup__GDPR_Compliance (if it all passes on CI pipeline, the env vars are being correctly loaded)

--- genai_agent_gdpr_compliance/deploy/AWS__Setup__GDPR_Compliance.py
+++ genai_agent_gdpr_compliance/deploy/AWS__Setup__GDPR_Compliance.py
@@ -1,6 +1,33 @@
-from osbot_utils.type_safe.Type_Safe import Type_Safe
+from osbot_aws.AWS_Config                           import AWS_Config
+from osbot_aws.aws.s3.S3                            import S3
+from osbot_utils.decorators.methods.cache_on_self   import cache_on_self
+from osbot_utils.type_safe.Type_Safe                import Type_Safe
 
-dns_entry= 'https://gdpr-compliance.dev.aws.cyber-boardroom.com'
+#dns_entry                    = 'https://gdpr-compliance.dev.aws.cyber-boardroom.com'       # todo: add workflow to set this up (note the hosted zone for dev.aws.cyber-boardroom.com is already configured in the 654654216424 aws account)
+
+GDPR_COMPLIANCE__PROJECT_NAME = "gdpr-compliance"
 
 class AWS__Setup__GDPR_Compliance(Type_Safe):
-    pass
+
+    @cache_on_self
+    def s3(self):
+        return S3()
+
+    @cache_on_self
+    def aws_config(self):
+        return AWS_Config()
+
+    def aws__account_id(self):
+        return self.aws_config().account_id()
+
+    def aws__configured(self):
+        return self.aws_config().aws_configured()
+
+    def aws__region_name(self):
+        return self.aws_config().region_name()
+
+    def s3__bucket__name(self):
+        return f"{GDPR_COMPLIANCE__PROJECT_NAME}--{self.aws__account_id()}--{self.aws__region_name()}"
+
+    def s3__bucket__exists(self):
+        return self.s3().bucket_exists(self.s3__bucket__name())
--- tests/integration/deploy/test_AWS__Setup__GDPR_Compliance.py
+++ tests/integration/deploy/test_AWS__Setup__GDPR_Compliance.py
@@ -1,5 +1,5 @@
-from unittest import TestCase
-
+from unittest                                                       import TestCase
+from osbot_utils.utils.Env                                          import load_dotenv
 from genai_agent_gdpr_compliance.deploy.AWS__Setup__GDPR_Compliance import AWS__Setup__GDPR_Compliance
 
 
@@ -7,9 +7,19 @@ class test_AWS__Setup__GDPR_Compliance(TestCase):
 
     @classmethod
     def setUpClass(cls):
+        load_dotenv()
         cls.aws_setup = AWS__Setup__GDPR_Compliance()
-
 
     def test__init__(self):
         with self.aws_setup as _:
             assert type(_) is AWS__Setup__GDPR_Compliance
+
+    def test_aws__configured(self):
+        assert self.aws_setup.aws__configured() is True
+
+    def test_s3_bucket_name(self):
+        assert self.aws_setup.s3__bucket__name() == "gdpr-compliance--654654216424--eu-west-1"
+
+    def test_s3_bucket__exists(self):
+        with self.aws_setup as _:
+            assert _.s3__bucket__exists() is False
